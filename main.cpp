29a8dbf5b8210294441e27236cdbfedf6381955d
TU <838004168@qq.com>
2025-03-25 21:20:33 +0800

    提交p4笔记

---
+++ main.cpp
@@ -0,0 +1,20 @@
+# include <iostream>
+# include "mul.hpp"
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    int result;
+
+    cout << "pick two integers:";
+    cin >> a;
+    cin >> b;
+
+    result = mul(a, b);
+
+    cout << "the result is" << result << endl;
+
+    return 0;
+}
