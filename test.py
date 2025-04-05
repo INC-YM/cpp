62cb54062329a6ccdf56e734a1af7654920caed9
TU <838004168@qq.com>
2025-04-05 13:19:52 +0800

    至p15

--- test.py
+++ test.py
@@ -1,2 +1,23 @@
-a = 17 / 5
-print(a)
+import heapq
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        # 注意 Python 默认的优先队列是小根堆
+        q = [(-nums[i], i) for i in range(k)]
+        heapq.heapify(q)
+
+        ans = [-q[0][0]]
+        for i in range(k, n):
+            heapq.heappush(q, (-nums[i], i))
+            while q[0][1] <= i - k:
+                heapq.heappop(q)
+            ans.append(-q[0][0])
+        
+        return ans
+nums = [1, -1]
+k = 1
+
+a = Solution()
+b = a.maxSlidingWindow(nums, k)
